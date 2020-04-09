73f41c8ef76a247ef8f182efefcb39569961b7c4
William Yeung <williamyeung1141998@gmail.com>
2020-04-08 23:48:39 -0400

    Initial project

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by William Yeung on 4/8/20.
+//  Copyright © 2020 William Yeung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
